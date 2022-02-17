fe6ea8cee8196eeb73a456fcc3f4685badbe034c
Ashish Patil <ashishpatil28032001@gmail.com>
2022-02-17 14:53:55 +0530

    NewsApi And Blogs⭐

---
+++ Blogs/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import *
+
+# Create your serializers here.
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = "__all__"
+        
